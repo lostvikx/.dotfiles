87c20d00087457604595d6a5e257e64e7231c917
Vikram Negi <viknegi0@gmail.com>
2025-10-04 02:59:05 +0530

    Bug fix: no -n option in batsignal.

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -7,5 +7,5 @@ feh -zr ~/Pictures/walls/ --bg-fill --no-fehbg
 picom --config ~/.config/picom/picom.conf -b
 redshift -c ~/.config/redshift/redshift.conf &
 
-batsignal -n -w 20 -c 10 -b
+batsignal -w 20 -c 10 -d 5 -b
 polybar main &
