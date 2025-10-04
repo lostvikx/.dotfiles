a264e1e98446a50942b0ddb5bbfaf8c146dddc5d
Vikram Negi <viknegi0@gmail.com>
2025-10-04 15:20:13 +0530

    Bug fix: allow bluetooth before toggling.

--- .config/polybar/scripts/bluetooth_toggle.sh
+++ .config/polybar/scripts/bluetooth_toggle.sh
@@ -4,7 +4,9 @@ BLUETOOTH_STATUS=$(bluetoothctl show | grep "Powered" | awk '{ print $2 }')
 
 if [ "$BLUETOOTH_STATUS" = "yes" ]; then
     bluetoothctl power off &> /dev/null
+    rfkill block bluetooth
 else
+    rfkill unblock bluetooth
     bluetoothctl power on &> /dev/null
 fi
 
