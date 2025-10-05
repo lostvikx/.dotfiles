7d87cd524d0404718f0c3407bcb46b517f3166d4
Vikram Negi <viknegi0@gmail.com>
2025-10-05 12:19:46 +0530

    Using xidlehook to auto-lock the system when idle.

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 dunst -config ~/.config/dunst/dunstrc &
-
 feh -zr ~/Pictures/walls/ --bg-fill --no-fehbg
 
 picom --config ~/.config/picom/picom.conf -b
@@ -9,3 +8,7 @@ redshift -c ~/.config/redshift/redshift.conf &
 
 batsignal -w 20 -c 10 -d 5 -b
 polybar main &
+
+xidlehook \
+    --not-when-audio --not-when-fullscreen \
+    --timer 300 'betterlockscreen --quiet --lock dimblur' '' &
