60b90d07ae760ad075463ea828e76f2798cc5041
Vikram Negi <viknegi0@gmail.com>
2025-11-05 05:04:30 +0530

    Disabled dpms and auto lock.

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 dunst -config ~/.config/dunst/dunstrc &
-#feh -zr ~/Pictures/walls/ --bg-fill --no-fehbg
-betterlockscreen -w
+
+# set the wallpaper
+betterlockscreen --wall
+#feh -zr ~/Pictures/walls/ --bg-fill --no-fehbg  # random wallpaper
 
 picom --config ~/.config/picom/picom.conf -b
 redshift -c ~/.config/redshift/redshift.conf &
@@ -10,6 +12,6 @@ redshift -c ~/.config/redshift/redshift.conf &
 batsignal -w 20 -c 10 -d 5 -b
 polybar main &
 
-xidlehook \
-    --not-when-audio --not-when-fullscreen \
-    --timer 300 'betterlockscreen --quiet --lock dimblur' '' &
+xset s off -dpms
+
+#xidlehook --not-when-audio --not-when-fullscreen --timer 300 'betterlockscreen --quiet --lock dimblur' '' &
