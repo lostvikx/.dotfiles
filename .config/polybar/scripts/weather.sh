bcac8ce96c4dcd3ecf80d29a44b3ccd8221baaca
Vikram Negi <viknegi0@gmail.com>
2026-01-28 00:27:59 +0530

    added cpu temps and floored weather temp to integer

--- .config/polybar/scripts/weather.sh
+++ .config/polybar/scripts/weather.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Wait for Wi-Fi connection.
-sleep 100
+#sleep 100
 
 # Cache the temperature info for 30 mins.
 cache="/tmp/weather.cache"
@@ -27,6 +27,8 @@ fi
 weather=$(curl -s "https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true")
 
 temp=$(echo $weather | jq -r ".current_weather.temperature")
+temp_int="${temp%.*}"
+
 weather_code=$(echo $weather | jq -r ".current_weather.weathercode")
 is_day=$(echo $weather | jq -r ".current_weather.is_day")
 
@@ -34,16 +36,16 @@ get_icon() {
     case $1 in
         0) 
             [[ "$is_day" -eq 1 ]] && echo "" || echo ""
-            ;;     # Clear sky
-        1|2) echo "" ;;   # Partly cloudy
-        3) echo "" ;;     # Cloudy
-        45|48) echo "" ;; # Fog
-        51|53|55) echo "" ;; # Drizzle
-        56|57|61|63|65) echo "" ;; # Rain
-        66|67) echo "" ;; # Freezing rain
-        71|73|75|77) echo "" ;; # Snow
-        95|96|99) echo "" ;; # Thunder
-        *) echo "" ;;    # Default (unknown)
+            ;;                       # Clear sky
+        1|2) echo "" ;;             # Partly cloudy
+        3) echo "" ;;               # Cloudy
+        45|48) echo "" ;;           # Fog
+        51|53|55) echo "" ;;        # Drizzle
+        56|57|61|63|65) echo "" ;;  # Rain
+        66|67) echo "" ;;           # Freezing rain
+        71|73|75|77) echo "" ;;     # Snow
+        95|96|99) echo "" ;;        # Thunder
+        *) echo "" ;;                # Default (unknown)
     esac
 }
 
@@ -52,5 +54,5 @@ icon=$(get_icon "$weather_code")
 if [[ -z "$temp" ]]; then
     echo '%{F#707880}N/A%{F-}'
 else
-    echo "${icon} ${temp}°C" | tee $cache
+    echo "%{T2}${icon}%{T-} ${temp_int}°C" | tee $cache
 fi
