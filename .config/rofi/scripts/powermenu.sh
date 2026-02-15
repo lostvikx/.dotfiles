a75fc9e64191e2bc4ed67b7e8acdaead49c05896
Vikram Negi <viknegi0@gmail.com>
2026-02-15 18:15:00 +0530

    updated rofi scripts to include font awesome icons

--- .config/rofi/scripts/powermenu.sh
+++ .config/rofi/scripts/powermenu.sh
@@ -2,22 +2,19 @@
 
 # More info: https://man.archlinux.org/man/extra/rofi/rofi-dmenu.5.en
 
-ACTION=$(echo "Hibernate,Lock,Reboot,Shutdown,Sleep" | rofi -no-show-icons -sep ',' -dmenu -p 'Action' -l 5 -i)
+ACTION=$(echo "   Lock |   Reboot |   Shutdown |   Sleep " | rofi -no-show-icons -sep '|' -dmenu -p 'Action' -l 4 -i)
 
 case "$ACTION" in
-    "Hibernate")
-        systemctl hibernate
-        ;;
-    "Lock")
+    "   Lock ")
         betterlockscreen --quiet --lock dimblur --off 30
         ;;
-    "Reboot")
+    "   Reboot ")
         reboot
         ;;
-    "Shutdown")
+    "   Shutdown ")
         poweroff
         ;;
-    "Sleep")
+    "   Sleep ")
         systemctl suspend
         ;;
     *)
--- .config/rofi/scripts/screenshot.sh
+++ .config/rofi/scripts/screenshot.sh
@@ -2,19 +2,19 @@
 
 # More info: https://man.archlinux.org/man/extra/rofi/rofi-dmenu.5.en
 
-ACTION=$(echo "Screenshot Selection to Clipboard,Screenshot Selection to File,Screenshot Fullscreen to Clipboard,Screenshot Fullscreen to File" | rofi -no-show-icons -sep ',' -dmenu -p 'Action' -l 4 -i)
+ACTION=$(echo "   Selection to Clipboard |   Selection to File |   Fullscreen to Clipboard |   Fullscreen to File " | rofi -no-show-icons -sep '|' -dmenu -p 'Action' -l 4 -i)
 
 case "$ACTION" in
-    "Screenshot Selection to Clipboard")
+    "   Selection to Clipboard ")
         maim --noopengl -s | xclip -selection clipboard -t image/png
         ;;
-    "Screenshot Selection to File")
+    "   Selection to File ")
         maim --noopengl -s ~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
         ;;
-    "Screenshot Fullscreen to Clipboard")
+    "   Fullscreen to Clipboard ")
         sleep 1 && maim --noopengl | xclip -selection clipboard -t image/png
         ;;
-    "Screenshot Fullscreen to File")
+    "   Fullscreen to File ")
         sleep 1 && maim --noopengl ~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
         ;;
     *)
